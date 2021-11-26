48bc73c62f619c92837f1fcad83e4d1f7f9522ff
EliStgoManz <eli.santiago.manzano@gmail.com>
2021-11-25 18:53:51 -0600

    Cambio de eli

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -270,9 +270,12 @@
             </div>
         </section>
 
-        <div id="mapid">
-            
-        </div>
+
+        <!--aqui inicia el div de mapa -->
+        
+         <div id="mapid">
+
+         </div>
 
 
 
@@ -662,7 +665,7 @@
 
         <script>
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([25.68192225636769, -100.24331641661017], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -673,20 +676,14 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1
 }).addTo(mymap);
 
-L.marker([51.5, -0.09]).addTo(mymap)
-    .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+L.marker([25.68192225636769, -100.24331641661017]).addTo(mymap)
+    .bindPopup("<b>Somos Fumyjar!</b><br />Servicios de fumigación y jardinería en MTY.").openPopup();
 
-L.circle([51.508, -0.11], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(mymap).bindPopup("I am a circle.");
 
-L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap).bindPopup("I am a polygon.");
+
+
+
+
 
 
 var popup = L.popup();
